980ab3e9a5b37de45d230d45f85efba9aa605fb9
PeeOops <liem.richiee@gmail.com>
2025-04-05 14:40:20 +0700

    Upd Js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,4 @@
 const singlePlayerMode = document.getElementById("singleplayer");
+const multiPlayerMode = document.getElementById("multiplayer");
+const playerDisplay = document.getElementById("player-display");
+const gameBoard = document.getElementById("game-board");
